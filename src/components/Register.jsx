924dd178ffa929883ee20464dd5dce03f44553f3
Rui  Martins <>
2024-05-13 21:23:15 +0100

    Form Register

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,11 +1,19 @@
+
+
 function Register(){
-    return(
+    return (
         <>
-            <div>
+        <div>
                 <h1>Register</h1>
+                <form>
+                    <input type="email" id="email" name="email" placeholder="Email"/>
+                    <input type="text" id="username" name="username" placeholder="Username"/>
+                    <input type="password" id="password" name="password" placeholder="Password"/>
+                </form>
             </div>
         </>
-    )
-}
+    )};
+
+
 
 export default Register
